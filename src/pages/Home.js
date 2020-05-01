1a240c441b533883ae31272d61a9f22f007fd9d4
gabrielgsv <gabriel.soares.vilela@gmail.com>
2020-05-01 10:24:52 -0300

    list categories in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Card } from "../components/Card";
+import { AntCard } from "../components/AntCard";
 import { FlexCenter } from "../components/Flex";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategories } from "../store/actions/category";
+import { Link } from "react-router-dom";
 
 export default () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
+
+  const categories = useSelector((state) => state.Category.categories);
   return (
     <FlexCenter>
-      <Card>
-        <h2>Tela Inicial</h2>
+      <Card
+        width="90vw"
+        height="90vh"
+        alignVertical="none"
+        alignHorizontal="center"
+      >
+        {categories &&
+          categories.map((category) => (
+            <Link to={`/category?drink=${category.strCategory}`}>
+              <AntCard>
+                <FlexCenter style={{ width: "100%", height: "100%" }}>
+                  <h2>{category.strCategory}</h2>
+                </FlexCenter>
+              </AntCard>
+            </Link>
+          ))}
       </Card>
     </FlexCenter>
   );
